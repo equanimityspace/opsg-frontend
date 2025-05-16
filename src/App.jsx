69aa201d433a99a67dc3cacdea534a3baa31ac03
Ryan <ryfly2016@gmail.com>
2025-05-16 08:54:15 -0600

    add useState to track userId and isAdmin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
+import { useState } from "react";
 import Home from "./Layout/Pages/Home";
 import Login from "./Layout/Pages/Login";
 import Registration from "./Layout/Pages/Registration";
@@ -10,43 +11,49 @@ import ContactForm from "./Layout/Pages/ContactForm";
 import React, { useContext } from "react";
 import AdminPage from "./Layout/Pages/AdminDashboard/AdminDashboard";
 
-
 import userNav from "./Features/Navigations/Navbars/UserNav";
 // import { AuthProvider } from "./Features/Navigations/AuthContext";
-
 
 // const AuthContext = React.createContext({ role: 'visitor'});
 
 function App() {
   // const { role } = useAuthState();
+  const [userId, setUserId] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(null);
 
   return (
     <>
-
       {/* Visitor Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login setUserId={setUserId} setIsAdmin={setIsAdmin} />}
+        />
         {/* <Route path="/unauthorized" element={<UnauthorizedPage/>} /> */}
         <Route path="/ourservices" element={<OurServices />} />
         <Route path="/contactform" element={<ContactForm />} />
 
+        {/* Protected Routes */}
+        <Route
+          path="/adminPage"
+          element={
+            <ProtectedRoutes isAdmin={true}>
+              <AdminPage />
+            </ProtectedRoutes>
+          }
+        ></Route>
 
-         {/* Protected Routes */}
-        <Route path = '/adminPage' element={ 
-          <ProtectedRoutes isAdmin={true}>
-            <AdminPage />
-          </ProtectedRoutes>}> 
-        </Route>
-
-
-        <Route path="/user/:userid" element={
-          <ProtectedRoutes isLoggedIn={true}>
-            <SingleUser />
-          </ProtectedRoutes>}> 
-        </Route>
-       </Routes>
+        <Route
+          path="/user/:userid"
+          element={
+            <ProtectedRoutes isLoggedIn={true}>
+              <SingleUser />
+            </ProtectedRoutes>
+          }
+        ></Route>
+      </Routes>
     </>
   );
 }
