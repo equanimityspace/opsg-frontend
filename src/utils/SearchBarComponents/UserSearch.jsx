e77024984753bff4cd7c81780dcf2b59b7e60abc
Tor <Torskaa@gmail.com>
2025-05-21 12:39:54 -0400

    User search perfected

--- src/utils/SearchBarComponents/UserSearch.jsx
+++ src/utils/SearchBarComponents/UserSearch.jsx
@@ -5,7 +5,7 @@ import { Card, Row, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import ReactiveButton from "reactive-button";
 
-export default function UserSearch() {
+export default function UserSearch({ showAllUsers, onSearch, onShowAll }) {
   const navigate = useNavigate();
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
@@ -33,6 +33,7 @@ export default function UserSearch() {
 
       const data = await response.json();
       setResults(data.users);
+      if (onSearch) onSearch();
     } catch (err) {
       console.error("Search failed:", err);
     }
@@ -45,57 +46,78 @@ export default function UserSearch() {
         onChange={(e) => setTerm(e.target.value)}
         placeholder="Search by name or email"
       />
-
-      <ReactiveButton
-        rounded
-        buttonState={loading ? "loading" : "idle"}
-        idleText={"Search"}
-        loadingText={"Loading"}
-        variant="secondary"
-        className="button3"
-        type="button"
-        onClick={handleSearch}
-        style={{
-          width: "80px",
-          fontSize: "12px",
-          backgroundColor: "#558e89",
-          marginBottom: "10px",
-        }}
-      >
-        Search
-      </ReactiveButton>
-
-      <Row xs={1} md={2} lg={3} className="g-4" style={{ marginTop: "5px" }}>
-        {results.map((user) => (
-          <Col key={user.id}>
-            <Card>
-              <Card.Body>
-                <Card.Title className="text-center">
-                  {user.firstName} {user.lastName}
-                </Card.Title>
-                <Card.Text className="text-center">({user.email})</Card.Text>
-              </Card.Body>
-              <Card.Footer className="text-center">
-                <ReactiveButton
-                  rounded
-                  buttonState={loading ? "loading" : "idle"}
-                  idleText={"View Profile"}
-                  loadingText={"Loading"}
-                  variant="secondary"
-                  className="button3"
-                  type="button"
-                  onClick={() => navigate(`user/${user.id}`)}
-                  style={{
-                    width: "160px",
-                    fontSize: "12px",
-                    backgroundColor: "#558e89",
-                  }}
-                ></ReactiveButton>
-              </Card.Footer>
-            </Card>
+      <Row>
+        <Col xs="auto">
+          <ReactiveButton
+            rounded
+            buttonState={loading ? "loading" : "idle"}
+            idleText={"SEARCH"}
+            loadingText={"Loading"}
+            variant="secondary"
+            className="button3"
+            type="button"
+            onClick={handleSearch}
+            style={{
+              width: "80px",
+              fontSize: "12px",
+              backgroundColor: "#558e89",
+              marginBottom: "10px",
+            }}
+          />
+        </Col>
+        {!showAllUsers && (
+          <Col xs="auto">
+            <ReactiveButton
+              onClick={onShowAll}
+              rounded
+              className="button3"
+              variant="secondary"
+              idleText="SHOW ALL USERS"
+              type="button"
+              style={{
+                marginRight: "5px",
+                width: "140px",
+                fontSize: "12px",
+                backgroundColor: "#558e89",
+              }}
+            />
           </Col>
-        ))}
+        )}
       </Row>
+
+      {!showAllUsers && (
+        <Row xs={1} md={2} lg={3} className="g-4" style={{ marginTop: "5px" }}>
+          {results.map((user) => (
+            <Col key={user.id}>
+              <Card>
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    {user.firstName} {user.lastName}
+                  </Card.Title>
+                  <Card.Text className="text-center">({user.email})</Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-center">
+                  <ReactiveButton
+                    rounded
+                    buttonState={loading ? "loading" : "idle"}
+                    idleText={"VIEW PROFILE"}
+                    loadingText={"Loading"}
+                    variant="secondary"
+                    className="button3"
+                    type="button"
+                    onClick={() => navigate(`user/${user.id}`)}
+                    style={{
+                      width: "160px",
+                      fontSize: "12px",
+                      backgroundColor: "#558e89",
+                    }}
+                  ></ReactiveButton>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 }
