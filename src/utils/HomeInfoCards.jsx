bb9dedf2ea15045b9e43cb5cc640629bf23e5c07
Ryan <ryfly2016@gmail.com>
2025-05-23 11:45:12 -0600

    even more capitalization issues

--- src/utils/HomeInfoCards.jsx
+++ src/utils/HomeInfoCards.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Card, CardBody } from "react-bootstrap";
-import stethoscope from "../Assets/img/stethoscope.png";
-import certificate from "../Assets/img/certificate.png"
-import medicalBag from "../Assets/img/medicalBag.png"
-
+import stethoscope from "../assets/img/stethoscope.png";
+import certificate from "../assets/img/certificate.png";
+import medicalBag from "../assets/img/medicalBag.png";
 
 const HomeInfoCards = () => {
   const cardData = [
@@ -17,24 +16,24 @@ const HomeInfoCards = () => {
       linkHint: "View my credentials",
     },
     {
-       src: stethoscope,
-       alt: "Stethoscope image",
-       className: "homeCards",
-       title: "Enrollments",
-       subtitle: "Streamlined onboarding",
-       text: "Our platform automates enrollments so you can focus on patient care.",
-       link: "/credentials",
-       linkHint: "View enrollment information",
+      src: stethoscope,
+      alt: "Stethoscope image",
+      className: "homeCards",
+      title: "Enrollments",
+      subtitle: "Streamlined onboarding",
+      text: "Our platform automates enrollments so you can focus on patient care.",
+      link: "/credentials",
+      linkHint: "View enrollment information",
     },
     {
-       src: medicalBag,
-       alt: "Medical Bag",
-       className: "homeCards",
-       title: "Consulting",
-       subtitle: "Expert guidance",
-       text: "Work one-on-one with our team of healthcare IT specialists.",
-       link: "/credentials",
-       linkHint: "Learn more about consulting",
+      src: medicalBag,
+      alt: "Medical Bag",
+      className: "homeCards",
+      title: "Consulting",
+      subtitle: "Expert guidance",
+      text: "Work one-on-one with our team of healthcare IT specialists.",
+      link: "/credentials",
+      linkHint: "Learn more about consulting",
     },
   ];
 
@@ -44,29 +43,38 @@ const HomeInfoCards = () => {
         <Card
           key={index}
           className="info-card"
-          style={{ backgroundColor: "#79cbbb",
-          margin: "3vw",
-           }}
+          style={{ backgroundColor: "#79cbbb", margin: "3vw" }}
         >
-          <CardBody style={{display: "flex",
-            flexDirection: "column",
-            maxWidth: "270px",
-            }}>
-            <Card.Img src={card.src} alt={card.alt} className="override"
+          <CardBody
             style={{
-              width: "25%",
-              display: "block",
-              alignSelf: "anchor-center",
-              paddingBottom: "4vh",
-              paddingTop: "3vh",
-            }}/>
-            <Card.Title
-            style={{
-              display: "block",
-              paddingBottom: "1em",
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: "270px",
             }}
-            >{card.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{card.subtitle}</Card.Subtitle>
+          >
+            <Card.Img
+              src={card.src}
+              alt={card.alt}
+              className="override"
+              style={{
+                width: "25%",
+                display: "block",
+                alignSelf: "anchor-center",
+                paddingBottom: "4vh",
+                paddingTop: "3vh",
+              }}
+            />
+            <Card.Title
+              style={{
+                display: "block",
+                paddingBottom: "1em",
+              }}
+            >
+              {card.title}
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {card.subtitle}
+            </Card.Subtitle>
             <Card.Text>{card.text}</Card.Text>
             {card.link && (
               <Card.Link href={card.link}>{card.linkHint}</Card.Link>
