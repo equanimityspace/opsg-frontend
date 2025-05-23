33910c363024ac4f0f64d8de65f8ff96f83c8adb
Tor <Torskaa@gmail.com>
2025-05-23 10:12:31 -0400

    Login fix

--- src/Layout/Pages/Login.jsx
+++ src/Layout/Pages/Login.jsx
@@ -25,8 +25,7 @@ export default function Login({ setUserId, setIsAdmin }) {
   const closeModal = () => setShow(false);
 
   //TESTING
-const [loading, setLoading] = useState(false);
-  
+  const [loading, setLoading] = useState(false);
 
   // stores data from login form
   const [formData, setFormData] = useState({
@@ -61,109 +60,113 @@ const [loading, setLoading] = useState(false);
 
   return (
     <>
- <div className="background">
-    <div className="backgroundAccent"></div>
-      <NavBar />
-      <div  style={{ paddingTop: '120px' }} className="d-flex justify-content-center vh-80">
-        {show ? (
-          <InfoModal
-            show={show}
-            hide={closeModal}
-            heading="Error"
-            body={response?.error?.data}
-          />
-        ) : (
-          <></>
-        )}
-        <Card className="w-50 mt-5" style={{display: "flex", }}>
-          <Card.Header>
-            <Nav variant="tabs" defaultActiveKey="/login">
-              <Nav.Item>
-                <Nav.Link
-                  href="/login"
-                  style={{
-                    variant: "secondary",
-                    fontSize: "12px",
-                    paddingBottom: "10px",
-                    paddingTop: "15px",
-                  }}
-                >
-                  LOGIN
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link
-                  href="/register"
-                  style={{
-                    variant: "secondary",
-                    fontSize: "12px",
-                    paddingBottom: "10px",
-                    paddingTop: "15px",
-                  }}
-                >
-                  REGISTER
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Card.Header>
-          <Card.Body>
-            <Form onSubmit={submit}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label
-                  className="formLabel"
-                  style={{ paddingLeft: "3px" }}
-                >
-                  EMAIL
-                </Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  placeholder="Enter email"
-                  onChange={update}
-                  style={{
-                    fontSize: "12px",
-                  }}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label
-                  className="formLabel"
-                  style={{ paddingLeft: "3px" }}
-                >
-                  PASSWORD
-                </Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  placeholder="Enter password"
-                  onChange={update}
-                  style={{
-                    fontSize: "12px",
-                  }}
-                />
-              </Form.Group>
+      <div className="background">
+        <div className="backgroundAccent">
+          <NavBar />
+          <div
+            style={{ paddingTop: "120px" }}
+            className="d-flex justify-content-center vh-80"
+          >
+            {show ? (
+              <InfoModal
+                show={show}
+                hide={closeModal}
+                heading="Error"
+                body={response?.error?.data}
+              />
+            ) : (
+              <></>
+            )}
+            <Card className="w-50 mt-5" style={{ display: "flex" }}>
+              <Card.Header>
+                <Nav variant="tabs" defaultActiveKey="/login">
+                  <Nav.Item>
+                    <Nav.Link
+                      href="/login"
+                      style={{
+                        variant: "secondary",
+                        fontSize: "12px",
+                        paddingBottom: "10px",
+                        paddingTop: "15px",
+                      }}
+                    >
+                      LOGIN
+                    </Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link
+                      href="/register"
+                      style={{
+                        variant: "secondary",
+                        fontSize: "12px",
+                        paddingBottom: "10px",
+                        paddingTop: "15px",
+                      }}
+                    >
+                      REGISTER
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Card.Header>
+              <Card.Body>
+                <Form onSubmit={submit}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label
+                      className="formLabel"
+                      style={{ paddingLeft: "3px" }}
+                    >
+                      EMAIL
+                    </Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="Enter email"
+                      onChange={update}
+                      style={{
+                        fontSize: "12px",
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label
+                      className="formLabel"
+                      style={{ paddingLeft: "3px" }}
+                    >
+                      PASSWORD
+                    </Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      placeholder="Enter password"
+                      onChange={update}
+                      style={{
+                        fontSize: "12px",
+                      }}
+                    />
+                  </Form.Group>
 
-          {/* // TESTING */}
-                <ReactiveButton
-                  rounded
-                  buttonState={loading ? 'loading' : 'idle'}
-                  idleText={'SUBMIT'}
-                  loadingText={'Loading'}
-                  variant="secondary"
-                  className="button3"
-                  type="submit"
-                  style={{
-                    width: "80px",
-                    fontSize: "12px",
-                    backgroundColor: "#558e89",
-                    // marginTop: "10px",
-                  }}>
-                </ReactiveButton>
-            </Form>
-          </Card.Body>
-        </Card>
+                  {/* // TESTING */}
+                  <ReactiveButton
+                    rounded
+                    buttonState={loading ? "loading" : "idle"}
+                    idleText={"SUBMIT"}
+                    loadingText={"Loading"}
+                    variant="secondary"
+                    className="button3"
+                    type="submit"
+                    style={{
+                      width: "80px",
+                      fontSize: "12px",
+                      backgroundColor: "#558e89",
+                      // marginTop: "10px",
+                    }}
+                  ></ReactiveButton>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
