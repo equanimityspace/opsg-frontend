81536148c5baa066b422062fa94261cf6930acff
Ryan <ryfly2016@gmail.com>
2025-05-23 10:58:44 -0600

    style components

--- src/Layout/Pages/UserDash/UserHeader.jsx
+++ src/Layout/Pages/UserDash/UserHeader.jsx
@@ -1,24 +1,42 @@
 import "./userHeader.css";
+import { UserNav } from "../../../Features/Navigations/Navbars/UserNav";
 import opsgLogo from "../../../assets/img/opsg-logo.png";
-import userHeader from "../UserDash/userHeader.css";
-
+import InfoCard from "../../../utils/InfoCard";
 import { Row, Col, Image, Container } from "react-bootstrap";
-import UserNav from "../../../Features/Navigations/Navbars/UserNav";
+import { useParams } from "react-router-dom";
+import Balance from "../../../utils/qbCustomer/Balance";
+import FullName from "../../../utils/qbCustomer/FullName";
 
 export default function UserHeader() {
+  const objId = useParams();
+  const id = objId.userId;
+
   return (
-    <div className="page" style={{ backgroundColor: "none" }}>
+    <div className="dashboard-wrapper">
       <UserNav />
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs={1} md={4}>
-            <Image src={opsgLogo} roundedCircle className="small-image" />
-          </Col>
-          <Col className="display-5 mt-4" md="auto">
-            OnPoint User View
+      <Container fluid className="dashboard-content">
+        <Row className="justify-content-center">
+          {/* Main Content Column */}
+          <Col xl={10} lg={11} className="main-content">
+            {/* Dashboard Header */}
+            <Row className="header-section mb-5 align-items-end">
+              <Col>
+                <div className="d-flex align-items-center">
+                  <Image
+                    src={opsgLogo}
+                    alt="OPSG Logo"
+                    width={60}
+                    className="me-3 logo-hover"
+                  />
+                  <h1 className="dashboard-title">
+                    <span className="fw-bold">OnPoint</span>
+                    <span className="fw-light"> User Dashboard</span>
+                  </h1>
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
-        <Row className="justify-content-md-center mt-4"></Row>
       </Container>
     </div>
   );
