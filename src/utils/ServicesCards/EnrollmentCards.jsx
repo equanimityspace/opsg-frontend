aea11406cd045f942c948d1d4d76c4dad9690294
Ryan <ryfly2016@gmail.com>
2025-05-23 12:47:40 -0600

    remove additional imports

--- src/utils/ServicesCards/EnrollmentCards.jsx
+++ src/utils/ServicesCards/EnrollmentCards.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import { Card, CardBody } from "react-bootstrap";
-import pen from "../../assets/img/pen.png";
 import { Card, CardBody, Row, Col, Container } from "react-bootstrap";
 import pen from "../../Assets/img/pen.png";
 
@@ -46,28 +43,41 @@ const EnrollmentCards = () => {
 
   return (
     <Container className="py-5 enrollment-card-container">
-        <Row className="justify-content-center">
-      {cardData.map((card, index) => (
-        <Col key={index} xs={12} sm={6} md={4} lg={3} xl={4} className="mb-4 justify-content-center d-flex">
-        <Card
-          className="info-card flex-grow-1"
-          style={{ backgroundColor: "#79cbbb", minHeight: "220px", maxWidth: "300px"}}
-        >
-          <CardBody className="d-flex flex-column align-items-center text-center">
-            <Card.Img 
-            src={card.src} 
-            alt={card.alt}
-            style={{
-              width: "25%",
-              paddingBottom: "4vh",
-              paddingTop: "3vh",
-            }}/>
-            <Card.Title>{card.title}</Card.Title>
-          </CardBody>
-         </Card>
-        </Col>
-      ))}
-     </Row>
+      <Row className="justify-content-center">
+        {cardData.map((card, index) => (
+          <Col
+            key={index}
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            xl={4}
+            className="mb-4 justify-content-center d-flex"
+          >
+            <Card
+              className="info-card flex-grow-1"
+              style={{
+                backgroundColor: "#79cbbb",
+                minHeight: "220px",
+                maxWidth: "300px",
+              }}
+            >
+              <CardBody className="d-flex flex-column align-items-center text-center">
+                <Card.Img
+                  src={card.src}
+                  alt={card.alt}
+                  style={{
+                    width: "25%",
+                    paddingBottom: "4vh",
+                    paddingTop: "3vh",
+                  }}
+                />
+                <Card.Title>{card.title}</Card.Title>
+              </CardBody>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 };
