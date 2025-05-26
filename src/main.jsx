a062ae108f5a9aa416112ae6ccc440ce5ae124c2
Ryan <ryfly2016@gmail.com>
2025-05-25 19:00:16 -0600

    switch from browserrouter to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./App/store.js";
 import ContextProvider from "./Features/Navigations/ContextProvider.jsx";
